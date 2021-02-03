45c5e4521ab388f822e06b265c121879f65a83a2
vtorres96 <victorcdc96@gmail.com>
2021-02-02 22:00:12 -0300

    adicionado model e controller com metodos: index, create e update da API

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const routes = require('./routes/index');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next){
-  res.send('Hello World')
-});
+const TaskController = require('../controllers/TaskController');
+
+router.get('/', TaskController.index);
+router.post('/', TaskController.create);
+router.put('/:id', TaskController.update);
+router.delete('/:id', TaskController.delete);
 
 module.exports = router;
