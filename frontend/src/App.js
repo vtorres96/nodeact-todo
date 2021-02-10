7f48f438b74f531cb3fa8c430ee1b3f993b663c2
vtorres96 <victorcdc96@gmail.com>
2021-02-09 22:02:34 -0300

    adicionado componente App

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import Header from './components/Header';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <Header />
+        <div className="main">
+          <TaskForm />
+          <TaskList />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
