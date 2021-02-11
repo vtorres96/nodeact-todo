48af060e27b3eb1a24179ad39b7631e83b97ff98
vtorres96 <victorcdc96@gmail.com>
2021-02-11 00:34:27 -0300

    updated header component

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Task Manager</h1>
+        </header>
+    )
+}
+
+export default Header
