96180aa9731903f187a3b2f56285885d0037c5f9
vtorres96 <victorcdc96@gmail.com>
2021-02-11 20:43:37 -0300

    validando caso nao exista nenhuma tarefa para listar

--- frontend/src/components/TaskList/index.js
+++ frontend/src/components/TaskList/index.js
@@ -4,23 +4,27 @@ import './styles.css';
 
 function TaskList(){
   let tasks = [
-    {
-      "id": 1,
-      "description": "Descrição 1"
-    },
-    {
-      "id": 2,
-      "description": "Descrição 2"
-    }
+    // {
+    //   "id": 1,
+    //   "description": "Descrição 1"
+    // },
+    // {
+    //   "id": 2,
+    //   "description": "Descrição 2"
+    // }
   ];
 
   return (
     <div>
-      <ul className="list-tasks">
-        {tasks.map(task => {
-          return <Task task={task} key={task.id}/>
-        })}
-      </ul>
+      {tasks.length ? (
+        <ul className="list-tasks">
+          {tasks.map(task => {
+            return <Task task={task} key={task.id}/>
+          })}
+        </ul>
+      ) : (
+        <div className="no-tasks">Não existe tarefas</div>
+      )}
     </div>
   );
 }
